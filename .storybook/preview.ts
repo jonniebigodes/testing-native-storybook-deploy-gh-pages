e8c6fe671f7297aa5ccb15b4eeafeeb49f21bfbb
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-12-03 19:16:15 +0000

    Screen section done

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,12 @@
 import type { Preview } from "@storybook/react-vite";
+
+import { initialize, mswLoader } from "msw-storybook-addon";
+
 import "../src/index.css";
+
+// Registers the msw addon
+initialize();
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,6 +16,7 @@ const preview: Preview = {
       },
     },
   },
+  loaders: [mswLoader],
 };
 
 export default preview;
